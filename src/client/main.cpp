253352908ef2047940cbc3cb2becc10c9ed61de6
brunomachado37 <bruno@DESKTOP-2JP0LD3.localdomain>
2022-09-25 16:23:48 +0200

    Resources addition

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -14,12 +15,29 @@ void testSFML() {
 using namespace std;
 using namespace state;
 
-int main(int argc,char* argv[])
-{
-    Exemple exemple;
-    exemple.setX(53);
+int main(int argc, char* argv[]){
 
-    cout << "It works !" << endl;
+    if(string(argv[1]) == "hello"){
+        //Exemple exemple;
+        //exemple.setX(53);
+        //testSFML();
 
-    return 0;
+        cout << "Tigris & Euphrates: An unofficial implementation" << endl;
+
+        return EXIT_SUCCESS;
+    }
+    else if(string(argv[1]) == "help"){
+        cout << "Tigris & Euphrates: An unofficial implementation" << endl;
+        cout << "Available arguments:" << endl;
+        cout << "hello      Print an introduction message" << endl;
+
+        return EXIT_SUCCESS;
+    }
+    else{
+        cout << "Unknown argument: " << argv[1] << endl;
+        cout << "Use the 'help' argument to check availble options." << endl;
+
+        return EXIT_SUCCESS;
+    }
+
 }
